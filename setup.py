67d9bd6f8097d81fbe8dadda12958c1ec3e71298
Yanga Lusiti <Yanga@explore-datascience.net>
2019-03-19 15:18:33 +0200

    first commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='pacckage-root',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://github.com/<YoungerCode>/<package-root>',
+    author='<Yanga Lusiti>',
+    author_email='<Yangalsiti@gmail.com>'
 )
